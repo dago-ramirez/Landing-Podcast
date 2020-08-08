d8c19e76cd36202b4d979826ff55e7c8390fa353
Dago R <dagoramirezserna@gmail.com>
2020-08-08 17:29:32 -0500

    Se corrigen aspectos de responsive

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-alert();
+let posicionYPrincipal = window.pageYOffset;
+console.log("posición y principal " + posicionYPrincipal)
+window.addEventListener("scroll", function() {
+    let posicionYActual = window.pageYOffset;
+    console.log("posición y actual " + posicionYActual)
+    if (posicionYPrincipal >= posicionYActual) {
+        document.getElementsByClassName("nav-section")[0].style.top = "0px"
+    }else {
+        document.getElementsByClassName("nav-section")[0].style.top = "-100px"
+    }
+    posicionYPrincipal = posicionYActual;
+})
+
+document.getElement
