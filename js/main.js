c0951bb0bfd454203f8ec89105f035fb46227c13
Dago R <dagoramirezserna@gmail.com>
2020-08-09 16:40:38 -0500

    Queda pendiente modificar la visualización la visualización del menú en dispositivos móviles

--- js/main.js
+++ js/main.js
@@ -1,14 +1,19 @@
-let posicionYPrincipal = window.pageYOffset;
-console.log("posición y principal " + posicionYPrincipal)
-window.addEventListener("scroll", function() {
-    let posicionYActual = window.pageYOffset;
-    console.log("posición y actual " + posicionYActual)
-    if (posicionYPrincipal >= posicionYActual) {
-        document.getElementsByClassName("nav-section")[0].style.top = "0px"
-    }else {
-        document.getElementsByClassName("nav-section")[0].style.top = "-100px"
-    }
-    posicionYPrincipal = posicionYActual;
-})
+// let posicionYPrincipal = window.pageYOffset;
+// console.log("posición y principal " + posicionYPrincipal)
+// window.addEventListener("scroll", function() {
+//     let posicionYActual = window.pageYOffset;
+//     console.log("posición y actual " + posicionYActual)
+//     if (posicionYPrincipal >= posicionYActual) {
+//         document.getElementsByClassName("nav-section")[0].style.top = "0px"
+//     }else {
+//         document.getElementsByClassName("nav-section")[0].style.top = "-100px"
+//     }
+//     posicionYPrincipal = posicionYActual;
+// })
 
-document.getElement
+let linksNav = document.querySelectorAll(".links-nav")[0];
+let conmutar = true;
+
+document.querySelectorAll(".hamburguer")[0].addEventListener("click", function(){
+    linksNav.classList.toggle("menu-expandido");
+})
